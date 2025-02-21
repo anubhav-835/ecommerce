8b60215a6f6beec21985cbacb16e1869d47827f2
anubhav-835 <anubhav835@gmail.com>
2025-02-21 16:21:19 +0530

    init commit

--- src/main/java/anubhav/restservice/customer_management/restcontroller/CustomerRestController.java
+++ src/main/java/anubhav/restservice/customer_management/restcontroller/CustomerRestController.java
@@ -1,5 +1,36 @@
 package anubhav.restservice.customer_management.restcontroller;
 
+import anubhav.restservice.customer_management.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class CustomerRestController {
 
+        @Autowired
+        private CustomerService userService;
+
+        @PostMapping("/signup")
+        public ResponseEntity<String> registerUser(@RequestBody Customer user) {
+            String response = userService.registerUser(user);
+            if (response.equals("User already exists")) {
+                return ResponseEntity.status(409).body(response);
+            }
+            return ResponseEntity.ok(response);
+        }
+
+        @PostMapping("/signin")
+        public ResponseEntity<Customer> signInUser(@RequestBody Customer user) {
+            try {
+                Customer authenticatedUser = userService.authenticateUser(user.getUseremail(), user.getPassword());
+                return ResponseEntity.ok(authenticatedUser);
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(401).body(null);
+            }
+        }
 }
